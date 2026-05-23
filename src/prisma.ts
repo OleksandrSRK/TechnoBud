15095f52631c356827be639eb413e12970c8552a
OleksandrSRK <oleksandr2005srk@gmail.com>
2026-05-23 03:08:59 +0300

    Switch to native TiDB/MySQL connection

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,24 +1,11 @@
 import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
-import { PrismaMariaDb } from '@prisma/adapter-mariadb'
-
-const dbUrl = new URL(process.env.DATABASE_URL!)
-
-const adapter = new PrismaMariaDb({
-    host: dbUrl.hostname,
-    port: Number(dbUrl.port || 3306),
-    user: dbUrl.username,
-    password: dbUrl.password,
-    database: dbUrl.pathname.slice(1),
-    connectionLimit: 10,
-})
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
 export const prisma =
     globalForPrisma.prisma ??
     new PrismaClient({
-        adapter,
         log: ['query', 'info', 'warn', 'error'],
     })
 
